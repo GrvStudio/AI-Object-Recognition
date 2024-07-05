0dde67b14b8161a8c51be75269f5432d4e9cdc17
Rahmad Iqbal <azcodia38@gmail.com>
2024-07-05 13:49:50 +0700

    update: code draw_border.py

--- utils/draw_border.py
+++ utils/draw_border.py
@@ -1,8 +1,14 @@
 import cv2
 
-def draw_border(img, top_left, bottom_right, color=(0, 255, 0), thickness=10, line_length_x=30, line_length_y=30):
+def draw_border(img, top_left, bottom_right, color=(0, 255, 0), thickness=10, line_length_x=30, line_length_y=30, padding=10):
     x1, y1 = top_left
     x2, y2 = bottom_right
+
+    # Tambahkan padding pada koordinat bounding box
+    x1 = max(0, x1 - padding)
+    y1 = max(0, y1 - padding)
+    x2 = min(img.shape[1], x2 + padding)
+    y2 = min(img.shape[0], y2 + padding)
 
     cv2.line(img, (x1, y1), (x1, y1 + line_length_y), color, thickness)  # top-left
     cv2.line(img, (x1, y1), (x1 + line_length_x, y1), color, thickness)
