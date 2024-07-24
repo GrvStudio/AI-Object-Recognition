0ebce4f578c71f3c4cefb5c74a0bcd47815909f4
Windu Nursetyadi <windudr@gmail.com>
2024-07-24 21:17:23 +0700

    use opencv-contrib's BarcodeDetector class

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cv2
 import os
 import time
+import numpy as np
 from ultralytics import YOLO
 from utils.draw_border import draw_border
 from utils.detect_bar_code import detect_bar_code, play_sound, show_barcode_results, update_barcode_count, get_total_unique_barcodes, count_png_files_in_no_barcode_folder
 from utils.saved_to_Csv import load_saved_barcodes, save_barcode_to_csv
 
 # Inisialisasi model YOLO yang sudah dilatih
-model = YOLO('./runs/detect/train/weights/best.pt')  # Ganti dengan path ke model yang sudah dilatih
+model = YOLO('./runs-ready/runs/detect/train/weights/best.pt')  # Ganti dengan path ke model yang sudah dilatih
 
 # Variabel global untuk menyimpan indeks unik objek "Cardboard"
 cardboard_index = 0
@@ -57,7 +58,40 @@ def capture_cardboard_if_no_barcode_detected(frame):
                 frame = draw_border(frame, (x1, y1), (x2, y2), color=border_color, thickness=2, line_length_x=30, line_length_y=30, padding=20)
             elif label == "Barcode":
                 barcode_detected = True
+                barcode_detector = cv2.barcode.BarcodeDetector()
                 overlay = frame.copy()
+                retval, points = barcode_detector.detect(overlay)
+                if retval:
+                    points = points.astype(np.int64)
+                    for i, point in enumerate(points):
+                        cv2.drawContours(overlay, [point], 0, (0, 255, 0), 2)
+                        cv2.addWeighted(overlay, 0.5, frame, 0.5, 0, frame)
+
+                # overlay = frame.copy()
+
+                # # equalize lighting
+                # gray = cv2.cvtColor(overlay, cv2.COLOR_BGR2GRAY)
+                # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+                # gray = clahe.apply(gray)
+
+                # # edge enhancement
+                # edge_enh = cv2.Laplacian(gray, cv2.CV_8U, ksize=3, scale=1, delta=0)
+
+                # blurred = cv2.bilateralFilter(edge_enh, 9, 75, 75)
+                # _, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+                # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
+                # closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+                # closed = cv2.erode(closed, None, iterations=4)
+                # closed = cv2.dilate(closed, None, iterations=4)
+
+                # contours, _ = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                # c = sorted(contours, key=cv2.contourArea, reverse=True)[0]
+                # rect = cv2.minAreaRect(c)
+                # box = cv2.boxPoints(rect)
+                # box = np.int0(box)
+                # cv2.drawContours(overlay, [box], -1, (0, 255, 0), 3)
+                # cv2.addWeighted(overlay, 0.5, frame, 0.5, 0, frame)
                 # alpha = 0.2  # Transparansi 70%
                 # cv2.rectangle(overlay, (x1, y1), (x2, y2), (0, 255, 0), -1)
                 # cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
@@ -78,7 +112,8 @@ def capture_cardboard_if_no_barcode_detected(frame):
         last_capture_time = current_time
 
 # Buka webcam
-cap = cv2.VideoCapture(0)  # 0 adalah indeks default untuk webcam internal
+# cap = cv2.VideoCapture(0)  # 0 adalah indeks default untuk webcam internal
+cap = cv2.VideoCapture(2)
 
 # Set pengaturan kamera untuk memastikan kecepatan frame tinggi
 cap.set(cv2.CAP_PROP_FPS, 60)
