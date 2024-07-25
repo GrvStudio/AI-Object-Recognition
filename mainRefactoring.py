e85926ae9d003c278eb2a325c1515c7e6cb80118
Rahmad Iqbal <azcodia38@gmail.com>
2024-07-25 13:07:03 +0700

    refactor(mainRefactoring.py): add new file MAIN refactoring

---
+++ mainRefactoring.py
@@ -0,0 +1,32 @@
+import cv2
+from object_detector.object_detector import ObjectDetector
+
+def main():
+    detector = ObjectDetector(model_path='./runs/detect/train/weights/best.pt')
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FPS, 60)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    if not cap.isOpened():
+        print("Error: Tidak dapat mengakses webcam.")
+        return
+
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Error: Tidak dapat membaca frame dari webcam.")
+                break
+
+            frame = detector.process_frame(frame, cap)
+            cv2.imshow('AI Prototype', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
