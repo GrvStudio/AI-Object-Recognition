c7383add1bacaf895ff8dba49f9683b9612069d8
Rahmad Iqbal <azcodia38@gmail.com>
2024-07-25 13:08:16 +0700

    fix: code comment

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from utils.detect_bar_code import detect_bar_code, play_sound, show_barcode_resu
 from utils.saved_to_Csv import load_saved_barcodes, save_barcode_to_csv
 
 # Inisialisasi model YOLO yang sudah dilatih
-model = YOLO('./runs-ready/runs/detect/train/weights/best.pt')  # Ganti dengan path ke model yang sudah dilatih
+model = YOLO('./runs/detect/train/weights/best.pt')  # Ganti dengan path ke model yang sudah dilatih
 
 # Variabel global untuk menyimpan indeks unik objek "Cardboard"
 cardboard_index = 0
@@ -66,38 +66,7 @@ def capture_cardboard_if_no_barcode_detected(frame):
                     for i, point in enumerate(points):
                         cv2.drawContours(overlay, [point], 0, (0, 255, 0), 2)
                         cv2.addWeighted(overlay, 0.5, frame, 0.5, 0, frame)
-
-                # overlay = frame.copy()
-
-                # # equalize lighting
-                # gray = cv2.cvtColor(overlay, cv2.COLOR_BGR2GRAY)
-                # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-                # gray = clahe.apply(gray)
-
-                # # edge enhancement
-                # edge_enh = cv2.Laplacian(gray, cv2.CV_8U, ksize=3, scale=1, delta=0)
-
-                # blurred = cv2.bilateralFilter(edge_enh, 9, 75, 75)
-                # _, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-                # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
-                # closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-                # closed = cv2.erode(closed, None, iterations=4)
-                # closed = cv2.dilate(closed, None, iterations=4)
-
-                # contours, _ = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                # c = sorted(contours, key=cv2.contourArea, reverse=True)[0]
-                # rect = cv2.minAreaRect(c)
-                # box = cv2.boxPoints(rect)
-                # box = np.int0(box)
-                # cv2.drawContours(overlay, [box], -1, (0, 255, 0), 3)
-                # cv2.addWeighted(overlay, 0.5, frame, 0.5, 0, frame)
-                # alpha = 0.2  # Transparansi 70%
-                # cv2.rectangle(overlay, (x1, y1), (x2, y2), (0, 255, 0), -1)
-                # cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
     
-    # if current_time - last_capture_time < 5:
-    #     return
     # Jika tidak ada barcode yang terdeteksi dan kedua objek "Cardboard" dan "ID Card" terdeteksi
     if len(cardboard_indices) > 0 and cardboard_detected and id_card_detected and not barcode_detected:
         capture_frame = True
@@ -113,7 +82,7 @@ def capture_cardboard_if_no_barcode_detected(frame):
 
 # Buka webcam
 # cap = cv2.VideoCapture(0)  # 0 adalah indeks default untuk webcam internal
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 
 # Set pengaturan kamera untuk memastikan kecepatan frame tinggi
 cap.set(cv2.CAP_PROP_FPS, 60)
@@ -141,15 +110,6 @@ try:
 
         # Deteksi QR code atau barcode
         _, barcode_info = detect_bar_code(frame.copy(), frame, cap)  # Copy frame untuk diproses
-
-        # Jika ada barcode yang terdeteksi, perbarui nilai terakhir jika berbeda
-        # if barcode_info:
-        #     new_barcode_data = ' | '.join([info[0] for info in barcode_info])
-        #     if new_barcode_data != last_barcode_data:
-        #         last_barcode_data = new_barcode_data
-        #         save_barcode_to_csv(last_barcode_data)  # Simpan data barcode ke CSV
-        #         update_barcode_count(last_barcode_data)  # Perbarui jumlah deteksi barcode
-        # Dapatkan jumlah total deteksi barcode yang berbeda
         if barcode_info:
             filtered_barcodes = [info[0] for info in barcode_info if "PJA2406220" in info[0]]
             if filtered_barcodes:
